f2c129232c6e4ae40ccec3e55fae9fceb8064df9
quyiming <quyiming@bytedance.com>
2019-07-13 20:11:33 +0800

    ownership

---
+++ 4.UnderstandingOwnership/what_is_ownership/src/main.rs
@@ -0,0 +1,7 @@
+// - Each value in Rust has a variable that’s called its owner.
+// - There can only be one owner at a time.
+// - When the owner goes out of scope, the value will be dropped.
+
+fn main() {
+    println!("Hello, world!");
+}
