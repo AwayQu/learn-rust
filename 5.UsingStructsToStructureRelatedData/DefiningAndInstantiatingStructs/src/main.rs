81797bf8c164b3c451741aaca1e9bdaf867db357
AwayQuEM <awayquym@gmail.com>
2019-07-15 00:45:35 +0800

    5.

---
+++ 5.UsingStructsToStructureRelatedData/DefiningAndInstantiatingStructs/src/main.rs
@@ -0,0 +1,38 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+let black = Color(0, 0, 0);
+let origin = Point(0, 0, 0);
+
+fn main() {
+    let user1 = User {
+    email: String::from("someone@example.com"),
+    username: String::from("someusername123"),
+    active: true,
+    sign_in_count: 1,
+    };
+    // println!("user: {}", user1);
+    println!("Hello, world!");
+
+    let user2 = User {
+    email: String::from("another@example.com"),
+    username: String::from("anotherusername567"),
+    ..user1
+    };
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email: email,
+        username: username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
