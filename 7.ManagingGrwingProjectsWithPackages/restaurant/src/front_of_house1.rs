26f8c3069eff50c52529828043311324db88b921
quyiming <quyiming@bytedance.com>
2019-07-26 23:07:20 +0800

    7.

---
+++ 7.ManagingGrwingProjectsWithPackages/restaurant/src/front_of_house1.rs
@@ -0,0 +1,9 @@
+
+
+// pub mod hosting {
+//     pub fn add_to_waitlist() {}
+// }
+
+// pub mod hosting;
+
+extern crate name;
--- 7.ManagingGrwingProjectsWithPackages/restaurant/src/lib.rs
+++ 7.ManagingGrwingProjectsWithPackages/restaurant/src/lib.rs
@@ -7,17 +7,82 @@ mod tests {
 }
 
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
 
-    mod serving {
+    pub mod serving {
         fn take_order() {}
 
         fn serve_order() {}
 
         fn take_payment() {}
+
+        pub mod back_of_house {
+            pub struct Breakfast {
+                pub toast: String,
+                seasonal_friute: String,
+            }
+
+            pub enum Appetizer {
+                Soup,
+                Salad,
+            }
+
+            impl Breakfast {
+                pub fn summer(toast: &str) -> Breakfast {
+                    return Breakfast {
+                        toast: String::from(toast),
+                        seasonal_friute: String::from("peaches"),
+                    }
+                }
+            }
+
+            fn fix_incorrect_order() {
+                cook_order();
+                super::serve_order();
+            }
+
+            fn cook_order() {}
+        }
     }
 }
+
+pub use crate::front_of_house::hosting;
+use crate::front_of_house::serving::back_of_house;
+use std::collections::HashMap;
+use std::collections::*;
+use std::io::{self, Write};
+
+pub use crate::front_of_house1::hosting;
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    meal.toast = String::from("Wheat");
+
+    println!("I'd like {} toast pleas", meal.toast);
+
+    let order1 = back_of_house::Appetizer::Soup;
+
+    let order2 = back_of_house::Appetizer::Salad;
+
+    hosting::add_to_waitlist();
+
+    hosting::add_to_waitlist();
+
+    hosting::add_to_waitlist();
+
+    let mut map = HashMap::new();
+    map.insert(1, 2);
+}
+
+
